e79814c5598d0712bba91243641ad4f501440de5
emreakkus03 <emreakkus003@gmail.com>
2026-04-08 16:31:39 +0200

    fixing vercel deployment error

--- civicsnap-dashboard/src/pages/city/reports/ReportDetail.tsx
+++ civicsnap-dashboard/src/pages/city/reports/ReportDetail.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { ID, Permission, Query, Role } from "appwrite";
+import { Query  } from "appwrite";
 import { databases, appwriteConfig, googleMapsApiKey, functions } from "@core/appwrite";
 import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
 
